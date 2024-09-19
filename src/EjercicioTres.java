eaa5a370686d1736f5d07b411633c26224ff2549
Usuario <castilla4454@gmail.com>
2024-09-19 17:51:32 +0200

    adasd

--- src/EjercicioTres.java
+++ src/EjercicioTres.java
@@ -3,6 +3,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class EjercicioTres {
 
@@ -18,34 +19,38 @@ public class EjercicioTres {
         String nombre = sc.nextLine();
         File fichero = new File(rutaFicheroOriginal+nombre);
 
-        escribirFicheroByte(fichero, nombre);
+        eliminarNumerosLinea(fichero, nombre);
 
     }
 
 
-    public static void escribirFicheroByte(File fichero, String nombre) {
+    public static void eliminarNumerosLinea(File fichero, String nombre) {
 
         try {
-            int lineas = 1;
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fichero));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/FicherosJava/copia" + nombre));
-            String linea;
-            while( (linea = bufferedReader.readLine()) != null ) {
-                bufferedWriter.write(lineas + linea+"\n");
-                lineas++;
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/FicherosJava/sinNumeros_" + nombre));
+
+            String line;
+            Pattern pattern = Pattern.compile("^\\d+\\.\\s*");
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String newLine = pattern.matcher(line).replaceFirst("");
+
+                bufferedWriter.write(newLine);
+                bufferedWriter.newLine();
             }
+
+            System.out.println("Archivo copiado y modificado correctamente.");
+
             bufferedReader.close();
             bufferedWriter.close();
-
-
-
-
+            System.out.println("El archivo sin números de línea ha sido creado.");
 
         } catch (FileNotFoundException e) {
-            LOGGER.error("ERROR " + "\n" + "El fichero no ha sido encontrado" + e.getMessage());
-
+            LOGGER.error("ERROR: El fichero no ha sido encontrado: " + e.getMessage());
         } catch (IOException ex) {
-            LOGGER.error(ex.getMessage());
+            LOGGER.error("ERROR: Ocurrió un error de I/O: " + ex.getMessage());
         }
     }
 }

b9f4ae6cce3afbdb78e65c0eb2d54bb382502e6f
Castilla454 <castilla4454@gmail.com>
2024-09-19 12:21:19 +0200

    adasd

---
+++ src/Modulo.java
@@ -0,0 +1,17 @@
+public class Modulo {
+
+    private String nombre;
+    private int numHoras;
+    private double nota;
+
+
+    public Modulo(String nombre, int numHoras, double nota) {
+        this.nombre = nombre;
+        this.numHoras = numHoras;
+        this.nota = nota;
+    }
+
+
+
+
+}

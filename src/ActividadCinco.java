68dcd4d9c53a91e7c48bbc701ebc61c9d1dd0676
Castilla454 <castilla4454@gmail.com>
2024-09-20 10:52:13 +0200

    adasd

---
+++ src/ActividadCinco.java
@@ -0,0 +1,73 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.Set;
+
+public class ActividadCinco {
+    static final Logger LOGGER = LogManager.getRootLogger();
+    public static final String RUTA_FICHERO = "src/FicherosJava/properties.cnf";
+    public static final String RUTA_FICHERO2 = "src/FicherosJava/properties2.cnf";
+
+    public static void main(String[] args) {
+
+        mostrarPropiedades(RUTA_FICHERO);
+        cargarPropiedades(RUTA_FICHERO2);
+    }
+
+
+    public static void mostrarPropiedades(String nombreFichero) {
+
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileInputStream(new File(nombreFichero)));
+
+            System.out.println("Todas las propiedades " + prop);
+            System.out.println("\n");
+
+            //Mostrar las propiedades de una a una
+            prop.list(System.out);
+            System.out.println("\n");
+
+            Set<Object> keys = prop.keySet();
+            System.out.println("Mi estilo: ");
+            for (Object key : keys) {
+                System.out.println(key + "--" + prop.getProperty(key.toString()));
+            }
+            System.out.println("\n");
+
+        } catch (FileNotFoundException e) {
+            LOGGER.error("ERROR " + "\n" + "El fichero no ha sido encontrado" + e.getMessage());
+
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage());
+        }
+
+    }
+
+    public static void cargarPropiedades(String nombreFichero) {
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileInputStream(new File(nombreFichero)));
+            prop.put("Color", "naranja");
+            prop.put("Valor", "19.5");
+            prop.put("Procedencia", "China");
+            prop.store(new FileOutputStream(new File(nombreFichero)), "Holis");
+
+
+            prop.list(System.out);
+
+        } catch (FileNotFoundException e) {
+            LOGGER.error("ERROR " + "\n" + "El fichero no ha sido encontrado" + e.getMessage());
+
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage());
+        }
+    }
+
+}

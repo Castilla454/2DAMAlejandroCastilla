13e441829fdd01c64e34a95a783660b20e3120d7
Castilla454 <castilla4454@gmail.com>
2024-10-28 09:28:51 +0100

    adasd

--- src/main/java/UD2/UnidadDos/JDBC_clave_autogenerada.java
+++ src/main/java/UD2/UnidadDos/JDBC_clave_autogenerada.java
@@ -8,6 +8,7 @@ import java.sql.*;
 public class JDBC_clave_autogenerada {
     private static final Logger LOGGER = LogManager.getRootLogger();
 
+
     public static void main(String[] args) {
 
         ConexionBD conexion = new ConexionBD();
